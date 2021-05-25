aada7ff062c5d587bc1051d062edbec114632aa6
taalhach <talhach891@gmail.com>
2021-05-25 14:55:49 +0000

    migrations

--- contrib/migrations/000001_init_schema.up.sql
+++ contrib/migrations/000001_init_schema.up.sql
@@ -7,3 +7,5 @@ CREATE TABLE users (
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     last_updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+
+INSERT INTO users  (uuid, role, email, password, created_at, last_updated_at) values ('180338c8-53a9-4305-6f40-c8e437a3806a', 'admin','admin@gmail.com', 'WmNxMMGGEH4S-1XFC_xFTNXHdtE=', now(), now())
